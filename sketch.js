0ff7da6ae97ee985852adb6da1653cb4b97e35c6
Hriddhiman17 <hriddhiman2008@gmail.com>
2021-05-18 16:15:40 +0530

    commit

---
+++ sketch.js
@@ -0,0 +1,28 @@
+var player, canvas, ground;
+
+function setup() {
+  canvas = createCanvas(700, 400);
+
+  player = createSprite(200, 100, 50, 80);
+  player.shapeColor = "green";
+
+  ground = createSprite(350, 375, 700, 20);
+
+}
+
+function draw() {
+  background(255);
+
+  if(keyDown('space') && player.velocityY === 0){
+    player.velocityY = player.velocityY-25
+  }
+  if(keyDown('left')){
+    player.x = player.x-10
+  }
+  if(keyDown('right')){
+    player.x = player.x+10
+  }
+  player.velocityY = player.velocityY + 1
+  drawSprites();
+  player.collide(ground);
+}
