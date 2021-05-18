4c2a4adfca9d1f1a2ac80d75465b4435da992745
Hriddhiman17 <hriddhiman2008@gmail.com>
2021-05-18 18:31:24 +0530

    commit

--- sketch.js
+++ sketch.js
@@ -1,30 +1,66 @@
-var player, canvas, ground;
+var player, alien, invisibleGround, ground, groundImg, deepGround, deepGroundImg, playerState;
+
+function preload() {
+  groundImg = loadImage("./Images/ground.png");
+  deepGroundImg = loadImage("./Images/deepGround.png")
+  alien = loadImage("./Images/Alien/Alien.png")
+  alienJumpRight = loadImage("./Images/Alien/AlienJump/AlienJumpRight.png")
+  alienJumpLeft = loadImage("./Images/Alien/AlienJump/AlienJumpLeft.png")
+}
 
 function setup() {
-  canvas = createCanvas(700, 400);
+  createCanvas(700, 400);
 
   player = createSprite(200, 100, 50, 80);
   player.shapeColor = "green";
 
-  ground = createSprite(350, 375, 700, 20);
+  invisibleGround = createSprite(350, 360, 400, 20);
+  invisibleGround.visible = false;
 
+  playerState = '';
 }
 
 function draw() {
   background(255);
 
-  if (keyDown('space') && player.velocityY === 0) {
-    player.velocityY = player.velocityY - 25;
+  if (playerState == 'runningLeft') {
+    console.log('Left')
+  } else if (playerState == 'runningRight') {
+    console.log('Right')
+  } else if (playerState == 'jumpRight'){
+    player.addImage(alienJumpRight)
+  } else if (playerState == 'jumpLeft'){
+    player.addImage(alienJumpLeft)
+  } else {
+    player.addImage(alien)
   }
-  if (keyDown('left')) {
+  // for (var i = 0; i < 500; i = i + 90) {
+  //   deepGround = createSprite(i, 390, 50, 20);
+  //   deepGround.addImage(deepGroundImg);
+  // }
+  // for (var i = 0; i < 500; i = i + 90) {
+  //   ground = createSprite(i, 375, 50, 20);
+  //   ground.addImage(groundImg);
+  // }
+  if (keyDown('space') && player.velocityY === 0 && player.y > 150) {
+    player.velocityY = player.velocityY - 15;
+  } else if (keyDown('left')) {
     player.x = player.x - 10;
-  }
-  if (keyDown('right')) {
+    playerState = 'runningLeft';
+  } else if (keyDown('right')) {
     player.x = player.x + 10;
+    playerState = 'runningRight';
+  } 
+  if (keyDown('space') && keyDown('right')){
+    playerState = 'jumpRight'
+  } else if (keyDown('space') && keyDown('left')){
+    playerState = 'jumpLeft'
+  } else {
+    playerState = '';
   }
   player.velocityY = player.velocityY + 1;
-  
+
   drawSprites();
 
-  player.collide(ground);
+  player.collide(invisibleGround);
 }
